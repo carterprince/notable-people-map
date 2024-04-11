478c2df1f8d6f77443053dd0642556dfe3abce54
Carter Prince <carteraprince@gmail.com>
2024-04-11 18:45:29 -0400

    Update map.py

--- map.py
+++ map.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import plotly.graph_objects as go
 
-df = pd.read_csv('cross-verified-database.csv', encoding='latin1')
+df = pd.read_csv('cross-verified-database.csv', encoding='ISO-8859-1')
 
-occupations = ['inventor', 'mathematician', 'physicist', 'philosopher', 'engineer', 'computer_scientist', 'biologist']
-colors = ['#d6c400', 'blue', 'red', 'purple', 'orange', 'black', 'lime']
+df['name'] = df['name'].apply(lambda x: x.encode('ISO-8859-1').decode('utf-8', 'ignore'))
 
-fig = go.Figure()
+occ_colors = {
+    'philosopher': 'purple',
+    'geneticist': 'green',
+    'biologist': 'lime',
+    'computer_scientist': 'black',
+    'inventor': '#d6c400',
+    'engineer': 'orange',
+    'mathematician': 'blue',
+    'physicist': 'red',
+}
 
-for occ, color in zip(occupations, colors):
-    #filtered_df = df[(df['level3_main_occ'] == occ) & (df['birth'] >= 1960)]
-    filtered_df = df[df['level3_main_occ'] == occ]
+def add_actual_trace(fig, occupation, color):
+    filtered_df = df[df['level3_main_occ'] == occupation]
     fig.add_trace(go.Scattergeo(
         lat=filtered_df['bpla1'],
         lon=filtered_df['bplo1'],
@@ -20,13 +27,35 @@ for occ, color in zip(occupations, colors):
             color=color,
             opacity=1
         ),
-        name=occ.capitalize().replace("_", " "),
+        name=occupation.capitalize().replace("_", " "),
         hovertemplate='%{text}<extra></extra>',
-        text=filtered_df['name'].str.replace('_', ' ')
+        text=filtered_df['name'].str.replace('_', ' '),
+        legendgroup=occupation, 
+        showlegend=False
     ))
 
+def add_dummy_trace(fig, occupation, color):
+    fig.add_trace(go.Scattergeo(
+        lat=[None],
+        lon=[None],
+        mode='markers',
+        marker=dict(
+            size=10,
+            color=color
+        ),
+        name=occupation.capitalize().replace("_", " "),
+        legendgroup=occupation,
+        showlegend=True
+    ))
+
+fig = go.Figure()
+
+for occ, color in occ_colors.items():
+    add_actual_trace(fig, occ, color)
+    add_dummy_trace(fig, occ, color)
+
 fig.update_layout(
-    title=dict(text='Birthplaces of notable inventors, mathematicians, and physicists', x=0.5, font=dict(size=24)),
+    title=dict(text='Birthplaces of notable people in STEM and Philosophy', x=0.5, font=dict(size=24)),
     showlegend=True,
     geo=dict(
         scope='world',
@@ -34,7 +63,9 @@ fig.update_layout(
         projection_type='natural earth',
         showland=True,
         landcolor='rgb(243, 243, 243)',
-        countrycolor='rgb(204, 204, 204)'
+        showcountries=True,
+        countrycolor='rgb(204, 204, 204)',
+        countrywidth=0.5
     )
 )
 
